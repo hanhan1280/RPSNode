c74f9f5fcc51eec1281deca5f25c89b11ee604b8
hanhan1280 <hanhan1280@gmail.com>
2020-01-29 19:34:53 -0500

    New commits

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+var http = require('http');
 var express = require('express');
-
 var app = express();
 
-app.get('/',function(req,res){
-    res.send('hello');
+const response = [
+    { id: 0, key: "Rock" },
+    { id: 1, key: "Paper" },
+    { id: 2, key: "Scissor" }
+]
+
+app.get('/', (req, res) => {
+    res.send("hello");
 })
 
-app.listen(8888);
+app.get('/rps', (req, res) => {
+    res.send(response);
+})
+
+app.get('/rps/:id', (req, res) => {
+    // 
+    let rpsValue = response.find(c => c.id === parseInt(req.params.id));
+    if (!rpsValue) {
+        res.status(404).send('No value was found');
+    }
+    res.send(rpsValue);    
+})
+
+const port = process.env.PORT || 8888;
+app.listen(port, () => {
+    console.log('Server started.');
+})
