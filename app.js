a0b25fe28d06689b46ba3ae5f0e7bfccd5a2ce1f
hanhan1280 <hanhan1280@gmail.com>
2020-01-30 01:12:34 -0500

    rest api

--- app.js
+++ app.js
@@ -4,49 +4,26 @@ var app = express();
 
 app.use(express.json());
 
-const turns = []
+const rpsVals = ["ROCK","PAPER","SCISSOR"];
 
 app.get('/', (req, res) => {
     res.send("hello");
 })
 
-app.get('/rps', (req, res) => {
-    res.send(turns);
-})
-
 app.post('/rps', (req, res) => {
-    const player = {
-        id: (turns.length%2==0)?1:2,
-        key: req.body.rpsValue
-    };
-    turns.push(player);   
-    const response = winLose();
-    res.send(response);
+    var index = getIndexOf(req.body.rpsValue.toUpperCase());
+    const winScenario = {
+        winCase: (index++>=rpsVals.length)?rpsVals[0]:rpsVals[index++]
+    }
+    res.send(winScenario);
 })
 
-function winLose(){
-    var winOne = false;
-    if(turns.length < 2){
-        return;
-    }
-    var player1 = turns.find(c=>c.id === 1);
-    var player2 = turns.find(c=>c.id === 2);
-    removeItems(turns,2);
-    if(player1.key == "Rock" && player2.key == "Scissor") winOne = true;    
-    else if(player1.key == "Scissor" && player2.key == "Paper") winOne = true;    
-    else if(player1.key == "Paper" && player2.key == "Rock") winOne = true;    
-    else if(player1.key == player2.key) return "noone wins";
-    
-    if(winOne){
-        return "Player 1 wins with " + player1.key;
-    }
-    return "Player 2 wins with " + player2.key;
-}
-
-function removeItems(arr, item) {
-    for ( var i = 0; i < item; i++ ) {
-        arr.pop();
-    }
+function getIndexOf(a){
+    for (let index = 0; index < rpsVals.length; index++) {
+        if(rpsVals[index] == a){
+            return index;
+        }        
+    } 
 }
 
 const port = process.env.PORT || 8888;
