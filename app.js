aa57bc6bf48e0978566f57d3db4a259937c1689f
hanhan1280 <hanhan1280@gmail.com>
2020-01-29 20:09:45 -0500

    handling get and post requests

--- app.js
+++ app.js
@@ -2,27 +2,25 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-const response = [
-    { id: 0, key: "Rock" },
-    { id: 1, key: "Paper" },
-    { id: 2, key: "Scissor" }
-]
+app.use(express.json());
+
+const turns = []
 
 app.get('/', (req, res) => {
     res.send("hello");
 })
 
 app.get('/rps', (req, res) => {
-    res.send(response);
+    res.send(turns);
 })
 
-app.get('/rps/:id', (req, res) => {
-    // 
-    let rpsValue = response.find(c => c.id === parseInt(req.params.id));
-    if (!rpsValue) {
-        res.status(404).send('No value was found');
-    }
-    res.send(rpsValue);    
+app.post('/rps', (req, res) => {
+    const player = {
+        id: (turns.length%2==0)?1:2,
+        key: req.body.rpsValue
+    };
+    turns.push(player);
+    res.send(player);
 })
 
 const port = process.env.PORT || 8888;
